5ae0dbd3bf88361c95287f81640c3c7c121bd3ba
Kecz <bartek8597@wp.pl>
2019-10-06 23:25:50 +0200

    Minor improvements and testing how to update scheduler

--- image_colorization/data_server.py
+++ image_colorization/data_server.py
@@ -3,7 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def load_cifar_10(path_to_cifar10):
+def load_cifar_10(path_to_cifar10, batch_size):
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -11,12 +11,12 @@ def load_cifar_10(path_to_cifar10):
     trainset = torchvision.datasets.CIFAR10(root=path_to_cifar10, train=True,
                                             download=True, transform=transform)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=0)
 
     testset = torchvision.datasets.CIFAR10(root=path_to_cifar10, train=False,
                                            download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=0)
 
     classes = ('plane', 'car', 'bird', 'cat',
