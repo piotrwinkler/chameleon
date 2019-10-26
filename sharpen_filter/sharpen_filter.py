728a4efdfed26a612559d34216a1a4007277a821
piotrwinkler <piotr329@gmail.com>
2019-10-26 20:23:36 +0200

    Add sharpen filter

--- sharpen_filter/sharpen_filter.py
+++ sharpen_filter/sharpen_filter.py
@@ -7,16 +7,14 @@ from base_classes.filter_sketch import FilterSketch
 from loguru import logger as log
 
 
-class CannyFilter(FilterSketch):
+class SharpenFilter(FilterSketch):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=5, padding=2)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, padding=1)
 
         log.info('NN structure defined!')
 
     def forward(self, x):
         x = f.relu(self.conv1(x))
-        x = f.relu(self.conv2(x))
 
         return x
