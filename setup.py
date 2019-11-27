02b6862ae0fdb231d22c28900b83ffc24404a4b0
piotrwinkler <piotr329@gmail.com>
2019-11-27 14:32:51 +0100

    Requirements polishing. setup.py for building project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="TorchFrame",
+    version="1.0",
+    author="Piotr Winkler in cooperation with Bartosz Bieliński",
+    description="Framework intended for learning and testing artificial neural networks.",
+    license="BSD",
+    keywords="framework, artificial intelligence, artificial neural networks",
+    packages=['base_classes'],
+)
